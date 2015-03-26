287a847f2276c794c9498d74fa89a13890c93866
Stefan Sundin <stefan@stefansundin.com>
2015-03-25 23:55:39 -0700

    Add program.

---
+++ ReplaceNotepad.bat
@@ -0,0 +1,15 @@
+@echo off
+@echo.
+if not exist "%~dp0sublime_text.exe" (
+  echo Warning: Can't find sublime_text.exe. You must put these files in your
+  echo Sublime Text directory before running this file.
+  echo Press enter to continue anyway...
+  @echo.
+  pause
+)
+echo If you see "ERROR: Access is denied." then you need to right click and use "Run as Administrator".
+@echo.
+echo Redirecting Notepad.exe to SublimeLauncher.exe...
+reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\notepad.exe" /v "Debugger" /t REG_SZ /d "\"%~dp0SublimeNotepadReplacer.exe\" -" /f
+@echo.
+pause
---
+++ SublimeNotepadReplacer.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <windows.h>
+#include <shlwapi.h>
+
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
+#define APP_NAME      L"Sublime Notepad Replacer"
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow) {
+  // Get path to sublime_text.exe
+  wchar_t path[MAX_PATH];
+  GetModuleFileName(NULL, path, ARRAY_SIZE(path));
+  PathRemoveFileSpec(path);
+  wcscat(path, L"\\sublime_text.exe");
+
+  if (!PathFileExists(path)) {
+    MessageBox(NULL, L"Could not find sublime_text.exe", APP_NAME, MB_ICONWARNING|MB_OK);
+    return 1;
+  }
+
+  // wchar_t logpath[MAX_PATH];
+  // GetModuleFileName(NULL, logpath, ARRAY_SIZE(logpath));
+  // PathRemoveFileSpec(logpath);
+  // wcscat(logpath, L"\\out.txt");
+  // FILE *f = _wfopen(logpath, L"ab");
+  // fwprintf(f, L"%s\n", lpCmdLine);
+  // fclose(f);
+
+  wchar_t *args = lpCmdLine;
+  if (*args == '-') {
+    args++;
+    args++;
+    while (*args != '\0') {
+      if (*args == ' ') {
+        args++;
+        break;
+      }
+      args++;
+    }
+    ShellExecute(NULL, NULL, path, args, NULL, nCmdShow);
+    return 0;
+  }
+
+  MessageBox(NULL, L"You have to setup the registry first, sorry.", APP_NAME, MB_ICONWARNING|MB_OK);
+
+  return 0;
+}
